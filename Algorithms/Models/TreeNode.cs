a5629ce218bc4a0e64a53fe7c32773d02cf7601a
andalexshap <a.shapovalov@infocom-s.ru>
2023-06-21 14:19:41 +0300

    fix

---
+++ Algorithms/Models/TreeNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Models
+{
+    public class TreeNode
+    {
+        public int value;
+        public Color color;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode? parent;
+
+        public TreeNode(int value)
+        {
+            this.value = value;
+            color = Color.Red;
+            left = null;
+            right = null;
+            parent = null;
+        }
+    }
+}
