09c03e28a9dcfa58f1d1788c30460033ef3b3275
Andalexshap <andalexshap@yandex.ru>
2023-10-23 22:51:55 +0300

    HW4

---
+++ sharp/Specialisation/HomeWork/HomeWork4/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         Дан массив и число. Найдите три числа в массиве сумма 
+        которых равна искомому числу. 
+        Подсказка: если взять первое число в массиве, можно ли найти 
+        в оставшейся его части два числа равных по сумме первому.
+        */
+
+        var nums = Enumerable.Range(0, 50).Select(i => i).ToArray();
+
+
+    }
+
+    public void FindTermOfTarget(IEnumerable<int> nums, int target)
+    {
+        var error = "Невозможно составить слагаемые из представленых чисел";
+
+        var firstPossibleTerm = nums.Where(f => f < target);
+        if (!firstPossibleTerm.Any())
+        {
+            var secondPossibleTerm =
+                firstPossibleTerm.Where(x => target - x < target);
+
+            if (!secondPossibleTerm.Any())
+            {
+
+            }
+        }
+
+        Console.WriteLine(error);
+
+    }
+}
