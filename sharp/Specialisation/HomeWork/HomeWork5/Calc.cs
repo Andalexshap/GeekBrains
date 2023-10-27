7c6746bcc2ce45b2ba0f799f4f7048456f71d986
Andalexshap <andalexshap@yandex.ru>
2023-10-27 22:28:22 +0300

    Add Calc

---
+++ sharp/Specialisation/HomeWork/HomeWork5/Calc.cs
@@ -0,0 +1,58 @@
+﻿namespace HomeWork5
+{
+    internal class Calc : ICalc
+    {
+        public double Result { get; set; } = 0D;
+        private Stack<double> LastResult { get; set; } = new Stack<double>();
+
+        public event EventHandler<EventArgs> MyEventHandler;
+
+        private void PrintResult()
+        {
+            MyEventHandler?.Invoke(this, new EventArgs());
+        }
+
+
+        public void Divide(int x)
+        {
+            Result /= x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Multy(int x)
+        {
+            Result *= x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Sub(int x)
+        {
+            Result -= x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Sum(int x)
+        {
+            Result += x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void CancelLast()
+        {
+            if (LastResult.TryPop(out double res))
+            {
+                Result = res;
+                Console.WriteLine("Последнее действие отменено. Результат равен:");
+                PrintResult();
+            }
+            else
+            {
+                Console.WriteLine("Невозможно отменить послдеднее действие!");
+            }
+        }
+    }
+}
