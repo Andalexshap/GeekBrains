1dd807ba94239a0406fb85e69763473757e382aa
Andalexshap <andalexshap@yandex.ru>
2023-10-28 22:40:41 +0300

    done

--- sharp/Specialisation/HomeWork/HomeWork5/Program.cs
+++ sharp/Specialisation/HomeWork/HomeWork5/Program.cs
@@ -1,7 +1,60 @@
-﻿internal class Program
+﻿using HomeWork5;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var calc = new Calc();
+        var action = "";
+        while (true)
+        {
+            Console.WriteLine($"Результат: {calc.Result} {action}");
+            if (string.IsNullOrEmpty(action))
+            {
+                Console.WriteLine($"Результат: Выберите действие:");
+            }
+            else
+            {
+                Console.WriteLine("Введите число");
+            }
+            var str = Console.ReadLine();
+            switch (str)
+            {
+                case "+": action = "+"; break;
+                case "-": action = "-"; break;
+                case "*": action = "*"; break;
+                case "/": action = "/"; break;
+                case "quit": Environment.Exit(0); break;
+
+                case "DevideByZero":
+                    Console.WriteLine("На ноль делить нельзя");
+                    break;
+                default:
+                    if (string.IsNullOrEmpty(action))
+                    {
+                        Console.WriteLine("Необходим выбор действия!");
+                        break;
+                    }
+                    if (int.TryParse(str, out int number))
+                    {
+                        calc.Result = action.Equals("+") ? calc.Result + number :
+                        calc.Result = action.Equals("-") ? calc.Result - number :
+                        calc.Result = action.Equals("*") ? calc.Result * number :
+                        calc.Result = action.Equals("/") && number != 0 ? calc.Result / number :
+                        calc.Result;
+
+                        action = string.Empty;
+                        Console.Clear();
+                        if (number == 0) goto case "DevideByZero";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не удалось преобразовать в число");
+                        break;
+                    }
+                    break;
+            }
+        }
     }
+
 }
