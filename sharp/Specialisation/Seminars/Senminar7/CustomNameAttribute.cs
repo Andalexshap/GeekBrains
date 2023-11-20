41a36b90bc7a86939374afeae8414e8aef060c40
Andalexshap <andalexshap@yandex.ru>
2023-11-20 21:03:22 +0300

    HW7

---
+++ sharp/Specialisation/Seminars/Senminar7/CustomNameAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Senminar7
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class CustomNameAttribute : Attribute
+    {
+        public string Name { get; }
+        public CustomNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
