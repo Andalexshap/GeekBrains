be566a4ec2cabae1b542957141e798ad54bd2c39
Andalexshap <andalexshap@yandex.ru>
2023-12-11 23:58:41 +0300

    fix

---
+++ sharp/Specialisation/Lections/Lections/Thread/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+
+    static void PrintThread()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - one");
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - two");
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - three");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Thread t = new Thread(PrintThread);
+            t.Start();
+        }
+    }
+}
---
+++ sharp/Specialisation/Seminars/Seminar_2_5/Entities/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Seminar_2_5.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<MessageEntity> Messages { get; set; }
+    }
+}
